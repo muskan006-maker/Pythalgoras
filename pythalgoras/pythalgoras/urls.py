1d910297669349beb81a0c32c8d2aceb2637ee90
Muskan <muskansharma94903@gmail.com>
2025-02-06 13:24:13 +0530

    a basic view

--- pythalgoras/pythalgoras/urls.py
+++ pythalgoras/pythalgoras/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pythalgoras.views import firstfunction
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', firstfunction),
 ]
